77c4968b9d1bc918a9eb7391379a31653303eaaa
Kacper Bojarczuk <kacper.bojarczuk1@gmail.com>
2023-06-20 21:55:06 +0200

    v2

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-#include <string>
-
-// + - * /  || trigonometric and logarithmic functions
-// classes and functions
-
-// w klasie sa funkcje obliczania
-// w main klasa, podaj liczbe, switch z case
+#include <cmath>
 
 class Calculator
 {
@@ -24,7 +18,42 @@ public:
     }
     double divide(double a, double b)
     {
-        return a / b;
+        if (b != 0)
+        {
+            return a / b;
+        }
+        else
+        {
+            std::cout << "Error: Divided by Zero" << std::endl;
+            return 0.0;
+        }
+    }
+    double sin(double a)
+    {
+        return std::sin(a);
+    }
+
+    double cos(double a)
+    {
+        return std::cos(a);
+    }
+
+    double tan(double a)
+    {
+        return std::tan(a);
+    }
+
+    double log(double a)
+    {
+        if (a > 0)
+        {
+            return std::log(a);
+        }
+        else
+        {
+            std::cout << "Error: Non-positive logarithm" << std::endl;
+            return 0.0;
+        }
     }
 };
 
@@ -38,35 +67,64 @@ int main()
     std::cout << "Enter first number: ";
     std::cin >> num1;
 
-    std::cout << "Choose symbol: + | - | * | / ";
+    std::cout << "Choose symbol: + | - | * | /  ||  s | c | t | l -> ";
     std::cin >> operation;
 
-    std::cout << "Enter second number: ";
-    std::cin >> num2;
+    bool triorlog = false; // Flaga informująca, czy wykonano operację trygonometryczną/logarytmiczną
 
-    double result;
-
-    switch (operation)
+    if (operation == 's' || operation == 'c' || operation == 't' || operation == 'l')
     {
-    case '+':
-        result = Calculator.add(num1, num2);
-        std::cout << "= " << result << std::endl;
-        break;
-    case '-':
-        result = Calculator.substract(num1, num2);
-        std::cout << "= " << result << std::endl;
-        break;
-    case '*':
-        result = Calculator.multiply(num1, num2);
-        std::cout << "= " << result << std::endl;
-        break;
-    case '/':
-        result = Calculator.divide(num1, num2);
-        std::cout << "= " << result << std::endl;
-        break;
-    default:
+        switch (operation)
+        {
+        case 's':
+            std::cout << "Sinus: " << Calculator.sin(num1) << std::endl;
+            triorlog = true;
+            break;
+        case 'c':
+            std::cout << "Cosinus: " << Calculator.cos(num1) << std::endl;
+            triorlog = true;
+            break;
+        case 't':
+            std::cout << "Tangent: " << Calculator.tan(num1) << std::endl;
+            triorlog = true;
+            break;
+        case 'l':
+            std::cout << "Logarithm: " << Calculator.log(num1) << std::endl;
+            triorlog = true;
+            break;
+        default:
+            std::cout << "Enter second number: ";
+            std::cin >> num2;
+            break;
+        }
+    }
+
+    if (!triorlog)
+    {
+        double result;
+
+        switch (operation)
+        {
+        case '+':
+            result = Calculator.add(num1, num2);
+            std::cout << "= " << result << std::endl;
+            break;
+        case '-':
+            result = Calculator.substract(num1, num2);
+            std::cout << "= " << result << std::endl;
+            break;
+        case '*':
+            result = Calculator.multiply(num1, num2);
+            std::cout << "= " << result << std::endl;
+            break;
+        case '/':
+            result = Calculator.divide(num1, num2);
+            std::cout << "= " << result << std::endl;
+            break;
+        default:
             std::cout << "ERROR" << std::endl;
             break;
+        }
     }
 
     return 0;
